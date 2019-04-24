f1e532ec284796a879e7aef8febd1faff97c9752
Scott Daniel <scott.daniel@barstowschool.org>
2019-04-24 14:01:14 -0500

    Here comes my Log Flume

---
+++ Log_Flume.java
@@ -0,0 +1,25 @@
+/**
+ * Write a description of class Log_Flume here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Log_Flume extends Attraction
+{
+
+    /**
+     * Constructor for objects of class Log_Flume
+     */
+    public Log_Flume(String name, double price)
+    {
+        super (name,price);
+
+    }
+
+    // Example of abstract method  - implementation
+    public void interact ()
+    {
+        System.out.print("To ride this attraction, it will cost you ");
+        System.out.println("$ " + getPrice() + ", " + getName());
+    }
+}
