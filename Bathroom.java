ed1739195dcf5b9c3ccaf55cfffcb62cdde9f1f4
Grayson Martin <grayson@graysonmartin.net>
2019-04-24 13:30:20 -0500

    Added the Bathroom class. Represents a bathroom in an AmusementPark.

---
+++ Bathroom.java
@@ -0,0 +1,17 @@
+/*
+ * Represents a Bathroom in an Amusement park.
+ * 
+ * @author Grayson Martin
+ */
+public class Bathroom extends Attraction {
+    static private int bathroomNumber = 0;
+    
+    public Bathroom() {
+        super("Bathroom " + bathroomNumber, 0.00);
+        bathroomNumber++;
+    }
+    
+    public void interact() {
+        System.out.println("Flushing sounds.");
+    }
+}
