55611a65bc238b0cefc8249b9a5d980a0158de61
TommyCuezze <tcuezze@gmail.com>
2019-04-24 13:44:40 -0500

    Add files via upload

---
+++ SuperSwing.java
@@ -0,0 +1,20 @@
+
+/**
+ * Simulates a giant swing.
+ *
+ * @author Tommy Cuezze
+ * @version 4/24/19
+ */
+public class SuperSwing extends Attraction
+
+{
+    public SuperSwing(String thisName, double thisPrice) {
+        super(thisName, thisPrice);
+    }
+    public void interact() {
+        System.out.println("You're now riding the " + ".");
+        System.out.println("It's a giant swing in the air.");
+        System.out.println("Here you go!");
+        System.out.println("WEEEEEEEEEE!!!");
+    }
+}
